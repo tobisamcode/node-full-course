f8e1c169cea025fd8b666e10c40c414ba05d8527
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-04-28 02:19:18 +0100

    node

--- math.js
+++
@@ -1,7 +0,0 @@
-exports.add = (a, b) => a + b;
-exports.subtract = (a, b) => a - b;
-exports.multiply = (a, b) => a * b;
-exports.divide = (a, b) => a / b;
-
-
-// module.exports = { add, subtract, multiply, divide }
