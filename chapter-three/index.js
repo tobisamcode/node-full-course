006dfe11d0195d89d367e0919d53fa0c2da7d98b
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-04-28 15:36:57 +0100

    chapter 4

--- chapter-three/index.js
+++ chapter-three/index.js
@@ -1,1 +1,16 @@
-console.log('testing');
+const logEvents = require('./logEvents');
+
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {};
+
+// initialize object
+const myEmitter = new MyEmitter();
+
+// add listener for the log event
+myEmitter.on('log', (msg) => logEvents(msg));
+
+setTimeout(() => {
+    // Emit event 
+    myEmitter.emit('log', 'Log event emitted!')
+}, 2000);
