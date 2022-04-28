753452c79b8d21c68d607c55722ed747bee5342b
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-04-28 02:19:26 +0100

    node

---
+++ chapter-one/math.js
@@ -0,0 +1,7 @@
+exports.add = (a, b) => a + b;
+exports.subtract = (a, b) => a - b;
+exports.multiply = (a, b) => a * b;
+exports.divide = (a, b) => a / b;
+
+
+// module.exports = { add, subtract, multiply, divide }
