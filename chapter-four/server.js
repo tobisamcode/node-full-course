52586ed71a8e5c9d308fcd3888c6a61580fa3556
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-04-28 15:36:52 +0100

    chapter 4

---
+++ chapter-four/server.js
@@ -0,0 +1,65 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {};
+
+// initialize object
+const myEmitter = new MyEmitter();
+
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    const extension = path.extension(req.url);
+
+    let contentType;
+
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+    }
+
+
+    // let path;
+
+    // switch (req.url) {
+    //     case '/':
+    //         res.statusCode = 200;
+    //         path = path.join(__dirname, 'views', 'index.html');
+    //         fs.readFile(path, 'utf-8', (err, data) => {
+    //             res.end(data);
+    //         });
+    //         break
+    // }
+
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+/*
+myEmitter.on('log', (msg) => logEvents(msg));
+
+myEmitter.emit('log', 'Log event emitted!')
+*/
