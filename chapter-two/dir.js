69f6ae0173a76facae06a124e37877072b03ce0c
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-04-28 12:53:16 +0100

    package

--- chapter-two/dir.js
+++ chapter-two/dir.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
+// it creates the directory if doesn't exist
 if (!fs.existsSync(path.join(__dirname, 'files', 'new'))) {
     fs.mkdir(path.join(__dirname, 'files', 'new'), (err) => {
         if (err) throw err;
@@ -8,6 +9,8 @@ if (!fs.existsSync(path.join(__dirname, 'files', 'new'))) {
     });
 }
 
+
+// it removes the directory if  exist
 if (fs.existsSync(path.join(__dirname, 'files', 'new'))) {
     fs.rmdir(path.join(__dirname, 'files', 'new'), (err) => {
         if (err) throw err;
