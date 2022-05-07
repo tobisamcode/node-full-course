523792d0bcc54cb999f32e065d8ac7315e651675
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-07 06:45:59 +0100

    chapter 4

--- chapter-four/server.js
+++ chapter-four/server.js
@@ -6,16 +6,25 @@ const fsPromises = require('fs').promises;
 
 const logEvents = require('./logEvents');
 const EventEmitter = require('events');
+class Emitter extends EventEmitter {};
+// initialize object
+const myEmitter = new Emitter();
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
 
 const PORT = process.env.PORT || 3500;
 
 const serveFile = async(filePath, contentType, response) => {
     try {
-        const data = await fsPromises.readFile(filePath, 'utf8');
-        response.writeHead(200, { 'Content-Type': contentType });
-        response.end(data);
+        const rawData = await fsPromises.readFile(filePath, !contentType.includes('image') ? 'utf8' : '');
+        const data = contentType === "application/json" ?
+            JSON.parse(rawData) : rawData;
+        response.writeHead(filePath.includes('404.html') ? 404 : 200, { 'Content-Type': contentType });
+        response.end(
+            contentType == "application/json" ? JSON.stringify(data) : data
+        );
     } catch (err) {
         console.log(err);
+        myEmitter.emit('log', `${req.name} : ${req.message}`, 'errLog.txt');
         response.statusCode = 500;
         response.end();
     }
@@ -23,6 +32,7 @@ const serveFile = async(filePath, contentType, response) => {
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
+    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
 
     const extension = path.extname(req.url);
 
@@ -94,11 +104,4 @@ const server = http.createServer((req, res) => {
     //         break
     // }
 });
-
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-/*
-myEmitter.on('log', (msg) => logEvents(msg));
-
-myEmitter.emit('log', 'Log event emitted!')
-*/
