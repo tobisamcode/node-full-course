1ac8b05a9c35aa1e327413c22dba5581a4ffa6e6
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-07 13:15:19 +0100

    chapter 4

--- chapter-four/logEvents.js
+++ chapter-four/logEvents.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
 
-const logEvents = async(message) => {
+const logEvents = async(message, logName) => {
     const datTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
     const logItem = `${datTime}\t${uuid()}\t${message}\n`;
     console.log(logItem);
@@ -13,7 +13,7 @@ const logEvents = async(message) => {
         if (!fs.existsSync(path.join(__dirname, 'logs'))) {
             await fsPromises.mkdir(path.join(__dirname, 'logs'))
         }
-        await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logItem);
+        await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
     } catch (err) {
         console.error(err);
     }
