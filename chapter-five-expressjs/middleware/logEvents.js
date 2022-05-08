6d5eb1a1d0573566e297df3fd886db34e8c7e2df
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-08 17:06:20 +0100

    middleware

---
+++ chapter-five-expressjs/middleware/logEvents.js
@@ -0,0 +1,23 @@
+const { format } = require("date-fns");
+const { v4: uuid } = require('uuid');
+
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const logEvents = async(message, logName) => {
+    const datTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+    const logItem = `${datTime}\t${uuid()}\t${message}\n`;
+    console.log(logItem);
+    try {
+        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'))
+        }
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logName), logItem);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+
+module.exports = logEvents;
