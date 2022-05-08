ff64d9f49431b0691bbdb8c8f4083d5ebd285190
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-08 17:25:29 +0100

    middleware

--- chapter-five-expressjs/server.js
+++ chapter-five-expressjs/server.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const path = require('path');
-const logEvents = require('./middleware/logEvents');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const { callbackify } = require('util');
 const app = express();
 const PORT = process.env.PORT || 3500;
 
 
 // custom middleware logger
-app.use((req, res, next) => {
-    logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt')
-    console.log(`${req.method} ${req.path}`);
-    next();
-})
+app.use(logger);
 
+// Cross Origin Resource Sharing
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 // built-in middleware  to handle urlencoded data
 // in other words, form data
