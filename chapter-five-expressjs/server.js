d73d70d74679093ca3736b589e71f8a284b0e787
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-08 17:50:01 +0100

    middleware

--- chapter-five-expressjs/server.js
+++ chapter-five-expressjs/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler')
 const { callbackify } = require('util');
 const app = express();
 const PORT = process.env.PORT || 3500;
@@ -14,7 +15,7 @@ app.use(logger);
 const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
 const corsOptions = {
     origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
@@ -56,9 +57,19 @@ app.get('/hello(.html)?', (req, res, next) => {
     res.send("Hello World!")
 });
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// app.use('/')
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: "404 Not found" });
+    } else {
+        res.type('txt').send("404 Not found");
+    }
 })
+
+app.use(errorHandler);
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
