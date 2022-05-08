ccfd3f0d424d4199658aba1391c55b215281de6c
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-08 17:25:35 +0100

    middleware

--- chapter-five-expressjs/middleware/logEvents.js
+++ chapter-five-expressjs/middleware/logEvents.js
@@ -19,5 +19,11 @@ const logEvents = async(message, logName) => {
     }
 }
 
+const logger = (req, res, next) => {
+    logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt')
+    console.log(`${req.method} ${req.path}`);
+    next();
+}
 
-module.exports = logEvents;
+
+module.exports = { logEvents, logger }
