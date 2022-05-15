2f5f4654ea808d82196a86dd9164df0cb82f8e4a
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-15 14:21:28 +0100

    creating the root routes

--- chapter-five-expressjs/server.js
+++ chapter-five-expressjs/server.js
@@ -38,28 +38,8 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
 // using the routes
+app.use('/', require('./routes/root'))
 app.use('/subdir', require('./routes/subdir'));
-
-app.get('^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-})
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html'); //302 by default
-})
-
-
-// Route Handlers
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log("attempted to load hello.html");
-    next()
-}, (req, res) => {
-    res.send("Hello World!")
-});
 
 // app.use('/')
 app.all('*', (req, res) => {
