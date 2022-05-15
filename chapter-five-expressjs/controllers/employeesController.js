afa44f217d8e88e3e3c56761ed5cfb76f6b9acde
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-15 16:56:34 +0100

    working with the api for delete

--- chapter-five-expressjs/controllers/employeesController.js
+++ chapter-five-expressjs/controllers/employeesController.js
@@ -24,11 +24,18 @@ const createNewEmployee = (req, res) => {
     res.status(201).json(data.employees);
 }
 
+
 const updateNewEmployee = (req, res) => {
-    res.json({
-        "firstname": req.body.firstname,
-        "lastname": req.body.lastname
-    })
+    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+    if (!employee) {
+        return res.status(400).json({ "message": `Employee ID ${req.body.id} not found.` })
+    }
+    if (res.body.firstname) employee.firstname = req.body.firstname;
+    if (res.body.lastname) employee.lastname = req.body.lastname;
+    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
+    const unsortedArray = [...filteredArray, employee];
+    data.setEmployees(unsortedArray.sort((a, b) => a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
+    res.join(data.employees);
 }
 
 const deleteEmployee = (req, res) => {
