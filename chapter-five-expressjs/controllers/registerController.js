2e3e196546cc79217ca2caa02dcfc4c5d232a7e5
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-16 23:21:22 +0100

    working with the api for authentication

--- chapter-five-expressjs/controllers/registerController.js
+++ chapter-five-expressjs/controllers/registerController.js
@@ -5,3 +5,11 @@ const userDB = {
 const fsPromises = require('fs').promises;
 const path = require("path");
 const bcrypt = require('bcrypt');
+
+const handlerNewUser = async(req, res) => {
+    const { user, password } = req.body;
+    if (!user || !password) return res.status(404).json({ "message": "username and password are required." })
+        // check for duplicate username in the db
+    const duplicate = usersDB.users.find(person => person.username === user);
+
+}
