f6620516b1680de9c4bc0159586c77492734dec9
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-17 05:38:24 +0100

    working with the api for authentication for registration

--- chapter-five-expressjs/routes/register.js
+++ chapter-five-expressjs/routes/register.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const registerController = require('../controllers/registerController');
 
-router.post('/')
+router.post('/', registerController.handlerNewUser);
+
+module.exports = router;
