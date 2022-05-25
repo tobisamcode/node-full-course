10c25a7cd0f6990ee62be29f799f0e80f63b8d2f
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-25 21:25:28 +0100

    jwt

--- chapter-five-expressjs/controllers/authController.js
+++ chapter-five-expressjs/controllers/authController.js
@@ -7,6 +7,12 @@ const userDB = {
 
 const bcrypt = require('bcrypt');
 
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+
 const handleLogin = async (req, res) => {
     const {user, password} = req.body;
     if (!user || !password) return res.status(400).json({ 'message': 'username and password are required.'});
@@ -17,9 +23,14 @@ const handleLogin = async (req, res) => {
     const match = bcrypt.compare(password, foundUser.password);
     if (match) {
         // creae  JWTs
+        const accessToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '30s' }
+        )
         res.json({ 'success': `user ${user} is logged in`});
     } else {
-        res.sendStatus(401); //nuauthorized
+        res.sendStatus(401); //unauthorized
     }
 }
 
