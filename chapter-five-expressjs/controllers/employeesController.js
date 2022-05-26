7d67c5e49aaee85f6f83a9b9ee057f0c7b2147d2
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-26 14:20:42 +0100

    JWT system

--- chapter-five-expressjs/controllers/employeesController.js
+++ chapter-five-expressjs/controllers/employeesController.js
@@ -1,60 +1,84 @@
 const data = {
-    employees: require('../model/employees.json'),
-    setEmployees: function(data) { this.employees = data }
+  employees: require("../model/employees.json"),
+  setEmployees: function (data) {
+    this.employees = data;
+  }
 };
 
 const getAllEmployees = (req, res) => {
-    res.json(data.employees);
-}
+  res.json(data.employees);
+};
 
 const createNewEmployee = (req, res) => {
-    const newEmployee = {
-        id: data.employees[data.employees.length - 1].id + 1 || 1,
-        firstname: req.body.firstname,
-        lastname: req.body.lastname
-    }
+  const newEmployee = {
+    id: data.employees[data.employees.length - 1].id + 1 || 1,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname
+  };
 
-    if (!newEmployee.firstname || !newEmployee.lastname) {
-        return res.status(400).json({
-            "message": "First and last names are required."
-        })
-    }
+  if (!newEmployee.firstname || !newEmployee.lastname) {
+    return res.status(400).json({
+      message: "First and last names are required."
+    });
+  }
 
-    data.setEmployees([...data.employees, newEmployee]);
-    res.status(201).json(data.employees);
-}
-
+  data.setEmployees([...data.employees, newEmployee]);
+  res.status(201).json(data.employees);
+};
 
 const updateNewEmployee = (req, res) => {
-    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
-    if (!employee) {
-        return res.status(400).json({ "message": `Employee ID ${req.body.id} not found.` })
-    }
-    if (req.body.firstname) employee.firstname = req.body.firstname;
-    if (req.body.lastname) employee.lastname = req.body.lastname;
-    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
-    const unsortedArray = [...filteredArray, employee];
-    data.setEmployees(unsortedArray.sort((a, b) => a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
-    res.json(data.employees);
-}
+  const employee = data.employees.find(
+    (emp) => emp.id === parseInt(req.body.id)
+  );
+  if (!employee) {
+    return res
+      .status(400)
+      .json({ message: `Employee ID ${req.body.id} not found.` });
+  }
+  if (req.body.firstname) employee.firstname = req.body.firstname;
+  if (req.body.lastname) employee.lastname = req.body.lastname;
+  const filteredArray = data.employees.filter(
+    (emp) => emp.id !== parseInt(req.body.id)
+  );
+  const unsortedArray = [...filteredArray, employee];
+  data.setEmployees(
+    unsortedArray.sort((a, b) => (a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
+  );
+  res.json(data.employees);
+};
 
 const deleteEmployee = (req, res) => {
-    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
-    if (!employee) {
-        return res.status(400).json({ "message": `Employee ID ${req.body.id} not found.` })
-    }
-    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
-    data.setEmployees([...filteredArray]);
-    res.json(data.employees);
-}
+  const employee = data.employees.find(
+    (emp) => emp.id === parseInt(req.body.id)
+  );
+  if (!employee) {
+    return res
+      .status(400)
+      .json({ message: `Employee ID ${req.body.id} not found.` });
+  }
+  const filteredArray = data.employees.filter(
+    (emp) => emp.id !== parseInt(req.body.id)
+  );
+  data.setEmployees([...filteredArray]);
+  res.json(data.employees);
+};
 
 const getEmployee = (req, res) => {
-    const employee = data.employees.find(emp => emp.id === parseInt(req.params.id));
-    if (!employee) {
-        return res.status(400).json({ "message": `Employee ID ${req.body.id} not found.` })
-    }
-    res.json(employee);
-}
+  const employee = data.employees.find(
+    (emp) => emp.id === parseInt(req.params.id)
+  );
+  if (!employee) {
+    return res
+      .status(400)
+      .json({ message: `Employee ID ${req.body.id} not found.` });
+  }
+  res.json(employee);
+};
 
-
-module.exports = { getAllEmployees, getEmployee, createNewEmployee, updateNewEmployee, deleteEmployee }
+module.exports = {
+  getAllEmployees,
+  getEmployee,
+  createNewEmployee,
+  updateNewEmployee,
+  deleteEmployee
+};
