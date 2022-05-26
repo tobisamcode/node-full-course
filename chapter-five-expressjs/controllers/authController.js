1983ea8b2e36db0345846f4ef7fddca023aa1763
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-26 21:47:38 +0100

    JWT system

--- chapter-five-expressjs/controllers/authController.js
+++ chapter-five-expressjs/controllers/authController.js
@@ -4,3 +4,27 @@ const userDB = {
     this.users = data;
   }
 };
+
+const bcrypt = require("bcrypt");
+
+const handleLogin = async (req, res) => {
+  const { user, password } = req.body;
+  if (!user || !password)
+    return res
+      .status(404)
+      .json({ "message": "username and password are required." });
+
+  const foundUser = userDB.users.find(person => person.username === user);
+
+  if (!foundUser) return res.sendStatus(401); // unauthourized
+
+  // evaluate password
+  const match = await bcrypt.compare(password, foundUser.password);
+  if (match) {
+    res.json({ success: `user ${user} is logged in!` });
+  } else {
+    res.sendStatus(401); // unauthorized
+  }
+};
+
+module.exports = { handleLogin };
