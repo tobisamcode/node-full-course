a0cb481cba690fb17c638106eae2b3b5897571b7
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-26 23:39:57 +0100

    JWT system

--- chapter-five-expressjs/controllers/authController.js
+++ chapter-five-expressjs/controllers/authController.js
@@ -9,6 +9,8 @@ const bcrypt = require("bcrypt");
 
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const fsPromises = require('fs').promises;
+const path = require('path');
 
 const handleLogin = async (req, res) => {
   const { user, password } = req.body;
