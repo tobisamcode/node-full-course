a6d9b4ea19d3796ea32297bdb0c72f9b08e4a79e
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-26 14:20:37 +0100

    JWT system

--- chapter-five-expressjs/server.js
+++ chapter-five-expressjs/server.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions')
-const { logger } = require('./middleware/logEvents');
-const errorHandler = require('./middleware/errorHandler');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 
-    // const { callbackify } = require('util');
+// const { callbackify } = require('util');
 const app = express();
 const PORT = process.env.PORT || 3500;
-
 
 // custom middleware logger
 app.use(logger);
@@ -22,35 +21,30 @@ app.use(cors(corsOptions));
 // 'content-type: application/x-www-form-urlencoded'
 app.use(express.urlencoded({ extended: false }));
 
-// built-in middleware for json 
+// built-in middleware for json
 app.use(express.json());
 
-
-
 // serve static files
-app.use('/', express.static(path.join(__dirname, '/public')));
-
+app.use("/", express.static(path.join(__dirname, "/public")));
 
 // using the routes
-app.use('/', require('./routes/root'));
+app.use("/", require("./routes/root"));
 
-app.use('/register', require('./routes/register'));
+app.use("/register", require("./routes/register"));
 
-
-
-app.use('/employees', require('./routes/api/employees'));
+app.use("/employees", require("./routes/api/employees"));
 
 // app.use('/')
-app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if (req.accepts('json')) {
-        res.json({ error: "404 Not found" });
-    } else {
-        res.type('txt').send("404 Not found");
-    }
-})
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not found" });
+  } else {
+    res.type("txt").send("404 Not found");
+  }
+});
 
 app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
