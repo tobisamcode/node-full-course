31e4efa5175d25da6be88612ad3df9d702b68037
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-28 14:19:33 +0100

    JWT system

--- chapter-five-expressjs/server.js
+++ chapter-five-expressjs/server.js
@@ -35,6 +35,7 @@ app.use("/", express.static(path.join(__dirname, "/public")));
 app.use("/", require("./routes/root"));
 app.use("/register", require("./routes/register"));
 app.use("/auth", require("./routes/auth"));
+app.use('/refresh', require('./routes/refresh'));
 
 app.use(verifyJWT);
 app.use("/employees", require("./routes/api/employees"));
