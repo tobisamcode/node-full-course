d9ff4cd5a5bfdc4c73992072b051d44e6e9f40c2
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-28 11:53:53 +0100

    JWT system

--- chapter-five-expressjs/routes/api/employees.js
+++ chapter-five-expressjs/routes/api/employees.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const router = express.Router();
-const path = require('path');
+const router = express.Router(); 
 const employeesController = require("../../controllers/employeesController");
 // const verifyJWT = require('../../middleware/verifyJWT');
 
