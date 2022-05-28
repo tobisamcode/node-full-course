f6d0856bb965ec7cf7bd43035c7941ea1af2975c
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-05-28 15:50:45 +0100

    JWT system

--- chapter-five-expressjs/middleware/errorHandler.js
+++ chapter-five-expressjs/middleware/errorHandler.js
@@ -1,6 +1,5 @@
 const { logEvents } = require('./logEvents');
 
-
 const errorHandler = (err, req, res, next) => {
     logEvents(`${err.name} : ${err.message}`, 'errLog.txt');
     console.error(err.stack)
