9109f3deed8037ee47ff489a8dc29e4c87dbda85
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-01 17:17:42 +0100

    JWT system and the log out

--- chapter-five-expressjs/routes/api/employees.js
+++ chapter-five-expressjs/routes/api/employees.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const router = express.Router(); 
+const express = require("express");
+const router = express.Router();
 const employeesController = require("../../controllers/employeesController");
+const ROLES_LIST = require("../../config/roles_list");
+const verifyRoles = require("../../middleware/verufyRoles");
 // const verifyJWT = require('../../middleware/verifyJWT');
 
-
-
-router.route('/')
+router
+    .route("/")
     .get(employeesController.getAllEmployees)
-    .post(employeesController.createNewEmployee)
-    .put(employeesController.updateNewEmployee)
-    .delete(employeesController.deleteEmployee);
+    .post(
+        verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
+        employeesController.createNewEmployee
+    )
+    .put(
+        verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
+        employeesController.updateNewEmployee
+    )
+    .delete(verifyRoles(ROLES_LIST.Admin), employeesController.deleteEmployee);
 
-router.route('/:id')
-    .get(employeesController.getEmployee);
+router.route("/:id").get(employeesController.getEmployee);
 
 module.exports = router;
