6e03b47332212870652adabf7d7cdbdeb3d92c6c
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-01 01:15:25 +0100

    JWT system and the log out

--- chapter-five-expressjs/config/corsOptions.js
+++ chapter-five-expressjs/config/corsOptions.js
@@ -1,11 +1,9 @@
-const whitelist = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500'
-];
+const allowedOrigins = require("./allowedOrigin");
+
+
 const corsOptions = {
     origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
