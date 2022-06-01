6a57a1a693458532d1d755e9e644943956cdc378
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-01 01:15:16 +0100

    JWT system and the log out

--- chapter-five-expressjs/server.js
+++ chapter-five-expressjs/server.js
@@ -6,12 +6,16 @@ const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
 const verifyJWT = require('./middleware/verifyJWT');
 const cookieParser = require('cookie-parser');
+const credentials = require('./middleware/credentials');
 // const { callbackify } = require('util');
 const app = express();
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
 app.use(logger);
+
+// Handle options credentials check - before CORS! and allow fetch cookies credentials requirement
+app.use(credentials);
 
 // Cross Origin Resource Sharing
 
