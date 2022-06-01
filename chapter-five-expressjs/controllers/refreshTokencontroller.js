bb44702b0e9a8cfb8da0faa2dea57fda02912d76
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-01 16:11:35 +0100

    JWT system and the log out

--- chapter-five-expressjs/controllers/refreshTokencontroller.js
+++ chapter-five-expressjs/controllers/refreshTokencontroller.js
@@ -1,43 +1,46 @@
 const userDB = {
-      users: require("../model/users.json"),
-      setUsers: function(data) {
+    users: require("../model/users.json"),
+    setUsers: function(data) {
         this.users = data;
-      }
-    };
-        
-    const jwt = require("jsonwebtoken");
-    require("dotenv").config();
-    
+    }
+};
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-    const handleRefreshToken = (req, res) => {
-      const cookies = req.cookies
-      console.log(cookies);
-      if (!cookies.jwt)
-        return res
-          .sendStatus(401);
-      console.log(cookies.jwt);
+const handleRefreshToken = (req, res) => {
+    const cookies = req.cookies;
+    console.log(cookies);
+    if (!cookies.jwt) return res.sendStatus(401);
+    console.log(cookies.jwt);
 
-      const refreshToken = cookies.jwt;
-      const foundUser = userDB.users.find(person => person.refreshToken === refreshToken);
-    
-      if (!foundUser) return res.sendStatus(403); // Forbidden
-      
-      // evaluate JWT
-      jwt.verify(
-            refreshToken,
-            process.env.REFRESH_TOKEN_SECRET,
-            (err, decoded) => {
-                  if (err || foundUser.username !== decoded.username) return res.sendStatus(403);
-                  const accessToken = jwt.sign(
-                        { "username": decoded.username },
-                        process.env.ACCESS_TOKEN_SECRET,
-                        { expiresIn: '30s' }
-                  );
-                  res.json({ accessToken });
-            }
-      );
-        // create JWTs
-       
-    };
-    
-    module.exports = { handleRefreshToken }
+    const refreshToken = cookies.jwt;
+    const foundUser = userDB.users.find(
+        person => person.refreshToken === refreshToken
+    );
+
+    if (!foundUser) return res.sendStatus(403); // Forbidden
+
+    // evaluate JWT
+    jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        (err, decoded) => {
+            if (err || foundUser.username !== decoded.username)
+                return res.sendStatus(403);
+            const roles = Object.values(foundUser.roles);
+            const accessToken = jwt.sign(
+                {
+                    UserInfo: {
+                        username: decoded.username,
+                        roles: roles
+                    }
+                },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: "30s" }
+            );
+            res.json({ accessToken });
+        }
+    );
+    // create JWTs
+};
+module.exports = { handleRefreshToken };
